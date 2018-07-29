7d00a5ecb7ad83c61fe03e8437d2643fb9345b98
Kamil Kindziuk <kamil.kindziuk@gmail.com>
2018-07-29 14:24:23 +0200

    add simple Movie model

--- djangoRestOmdb/settings.py
+++ djangoRestOmdb/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'moviesApi'
+    'rest_framework',
+    'moviesApi',
 ]
 
 MIDDLEWARE = [
--- moviesApi/models.py
+++ moviesApi/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+  title = models.CharField(max_length=200)
+  year = models.IntegerField()
+  plot = models.TextField()
+
+  def __str__(self):
+    return self.title
+
