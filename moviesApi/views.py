d45a8c8c3d8b0ed50ba304e482f13129c034fec8
Kamil Kindziuk <kamil.kindziuk@gmail.com>
2018-07-30 23:40:20 +0200

    add imports in views

--- moviesApi/views.py
+++ moviesApi/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+import requests
 
 from .models import Movie
 from .serializers import MovieSerializer
