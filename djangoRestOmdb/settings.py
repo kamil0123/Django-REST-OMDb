4b6664a241a1ad1f6a64e7ecfa98bf2e14c799c8
Kamil Kindziuk <kamil.kindziuk@gmail.com>
2018-08-01 21:47:17 +0200

    Reset settings file to run locally

--- djangoRestOmdb/settings.py
+++ djangoRestOmdb/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
+import os
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -23,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ie!jv*ln4!@lg-^v9y3=4uc1w+0_3#^jh7d!ye*iv5n0c=nm)('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
@@ -127,4 +128,4 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# DATABASES['default'].update(db_from_env)
